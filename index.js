1f255e29015371b7bb2692d012bfdeeca7c70996
Daphineatim <hopedaphine19@gmail.com>
2022-03-10 17:54:58 +0300

    Rename background image, Add cancel image, Update index.html, Update style.css, Update mobile.css, Update index.html

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-// document.querySelectorAll('.mobile-humberger').forEach(menu => {
-//     menu.addEventListener('click', (event) =>  {
-//     menu.classList.toggle('open')
-// })
-// })
+const menu = document.querySelector('.menu-icon');
+const humberger =document.querySelector('#humberger');
+const cancel = document.querySelector('.cancel');
 
-//Create veriable that contains DOM elements(1.hamburger button-2.cross button) list items to show up
-// container(mobile menu class)
-const humberger = document.getElementById('mobile-humberger');
-const close = document.getElementById('close');
-const menuList = document.getElementById('nav-menu');
-const menuItems = document.querySelectorAll('nav-list'); // output ==> array
-const navbarContainer = document.getElementById('navbar flex');
-//humburger.clickevent ===> display container block
-//cross.clickevent ===> display container none
-//<li> clickevent ===> display container none</li>
-function openNavBar () {
-    navbarContainer.style.display='block';
-}
-function closeNavBar (){
-  navbarContainer.style.display='none';
-}
-humberger.addEventListener('click',openNavBar);
-close.addEventListener('click',closeNavBar);
-//for each item element display none
-//for each element of the list item add a clik event listener that makes container display none
-//exp:
-// array1.forEach(element => console.log(element));
-// // expected output: "a"
-// // expected output: "b"
-// // expected output: "c"
-menuItems.forEach((element) =>{
- element.addEventListener('click',closeNavBar)
+humberger.addEventListener('click',  (event) => {
+  console.log(menu.classList)
+  menu.classList.remove('menu-toggle');
+  cancel.classList.remove('hide');
+   humberger.classList.add('hide');
 });
 
+cancel.addEventListener('click', e => {
+  menu.classList.add('hide');
+  // cancel.classList.add("hide")
+  // humberger.classList.add("display");
+})
