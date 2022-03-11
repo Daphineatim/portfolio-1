039154e5e5781344240d2680440a09e5766d5524
Daphineatim <hopedaphine19@gmail.com>
2022-03-12 02:03:28 +0300

    Update index.html, index.js, mobile.css, style.css

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
+// const humberger = document.querySelector('.humberger');
+// const cancel = document.querySelector('#cancel');
+// const navigation = document.querySelector('.menu-icon');
+// const portfolio = document.querySelector('.menu-link');
+// const about = document.querySelector('.menu-link');
+// const contact = document.querySelector('.menu-link');
+
+// humberger.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   navigation.classList.toggle('show');
+//   humberger.classList.add('hide');
+//   cancel.classList.remove('hide');
+//   portfolio.classList.add('menu-link');
+//   about.classList.add('menu-link');
+//   contact.classList.add('menu-link');
+// });
+
+// cancel.addEventListener('click', () => {
+//   humberger.classList.remove('hide');
+//   navigation.classList.remove('show');
+// });
+
+// portfolio.addEventListener('click', (e) => {
+//   e.stopPropagation();
+
+//   navigation.classList.remove('show');
+//   humberger.classList.remove('hide');
+// });
+
+// about.addEventListener('click', (e) => {
+//   e.stopPropagation();
+
+//   navigation.classList.remove('show');
+//   humberger.classList.remove('hide');
+// });
+
+// contact.addEventListener('click', (e) => {
+//   e.stopPropagation();
+
+//   navigation.classList.remove('show');
+//   humberger.classList.remove('hide');
+// });
+
 const menu = document.querySelector('.menu-icon');
 const humberger = document.querySelector('#humberger');
+// const sideBar = document.querySelector('.sidebar')
 const cancel = document.querySelector('.cancel');
 
 humberger.addEventListener('click', () => {
