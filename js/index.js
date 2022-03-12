c138998883a6fbc61f0ae57eeaa5a8ab6df965d8
Daphineatim <hopedaphine19@gmail.com>
2022-03-12 19:18:22 +0300

    Update index.js, style.css, Fix linters

--- js/index.js
+++ js/index.js
@@ -17,3 +17,30 @@ document.querySelectorAll('.menu-link').forEach((link) => link.addEventListener(
   sideBar.classList.remove('active');
   wrapper.classList.remove('show');
 }));
+
+// preserve data
+const formContainer = document.querySelector('.contact_container');
+const emailValue = document.querySelector('.email');
+const messageError = document.querySelector('.errorMessage');
+
+formContainer.addEventListener('submit', (e) => {
+  if (emailValue.value === emailValue.value.toLowerCase()) {
+    messageError.textContent = '';
+    const formData = {
+      name: document.querySelector('name').value,
+      email: document.querySelector('email').value,
+      message: document.querySelector('message').value,
+    };
+    localStorage.setItem('formData', JSON.stringify(formData));
+  } else {
+    e.preventDefault();
+    messageError.innerHTML = '*Write your email in lower case <br> * form invalid';
+  }
+});
+
+if (formContainer) {
+  const { name, email, message } = JSON.parse(localStorage.getItem('formData'));
+  document.querySelector('name').value = name;
+  document.querySelector('email').value = email;
+  document.querySelector('message').value = message;
+}
